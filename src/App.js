dc88d3f30cee1829208b07db250c84c1e207eb68
Crissy Cabrera <crissymichelle@proton.me>
2024-01-28 11:26:10 -1000

    added dfacs route to app.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { AuthProvider, useAuth } from './components/AuthContext';
 import AuthRoute from './routes/AuthRoute';
 import NavBar from './components/NavBar';
 import HomeRoute from './routes/HomeRoute';
+import DfacsRoute from './routes/DfacsRoute';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Routes>
             <Route path="/auth/customer/login" element={<AuthRoute />} />
             <Route path="/" element={<HomeRoute />} />
+            <Route path="/auth/dfacs" element={<DfacsRoute />} />
           </Routes>
         </Router>
       </AuthProvider>
--- src/routes/DfacsRoute.js
+++ src/routes/DfacsRoute.js
@@ -1,2 +1,61 @@
 import React, { useState, useEffect } from "react";
 import DdashApi from "../api";
+import DfacCard from "../components/DfacCard";
+
+function DfacsRoute() {
+    const [dfacs, setDfacs] = useState(null);
+    // search and filter managed in local state
+    const [searchTerm, setSearchTerm] = useState("");
+    const [addressTerm, setAddressTerm] = useState("");
+    const [hoursTerm, setHoursTerm] = useState("");
+    const [errors, setErrors] = useState(null);
+
+    function handleSearch(event) {
+        event.preventDefault();
+        fetchDfacs(); // calls API onSubmit
+    }
+
+    const fetchDfacs = async () => {
+        try {
+            const filters = {
+                nameLike: searchTerm || undefined,
+                addressLike: addressTerm || undefined,
+                hoursLike: hoursTerm || undefined
+            };
+
+            const dfacData = await DdashApi.getDfacs(filters);
+            setDfacs(dfacData);
+        } catch (err) {
+            console.error("Error fetching dfacs: ", err);
+            setErrors(err);
+        }
+    };
+
+    useEffect(() => {
+        fetchDfacs();
+    }, []);
+    if (errors) return <p>Error loading DFACs</p>;
+    if (!dfacs) return <p>Loading DFACs ... ...</p>;
+
+    return (
+        <div>
+            <h1>Dining Facilities</h1>
+            <form onSubmit={handleSearch}>
+                <input
+                    type="text"
+                    placeholder="Search DFACs by name, address, or hours"
+                    value={searchTerm}
+                    onChange={e => setSearchTerm(e.target.value)}
+                />
+                <button type="submit">Search</button>
+            </form>
+            <div>
+                {dfacs.map(dfac => (
+                    <DfacCard key={dfac.dfacID} dfac={dfac} />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default DfacsRoute;
