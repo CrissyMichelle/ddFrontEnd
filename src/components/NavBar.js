8dc9530097fb8880c90514168ee357eda289d927
Crissy Cabrera <crissymichelle@proton.me>
2024-01-28 10:03:53 -1000

    styled NavBar and added logo images

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from "./AuthContext";
+import "./NavBar.css";
+import logo from "../images/LL_Logo.png"
+import divLogo from "../images/25IDlogo.png";
+import healthLogo from "../images/H2F.jpeg";
 
 function NavBar() {
     const { isLoggedIn, currentUser, logout } = useAuth();
     const navigate = useNavigate();
+    const [isScreenSmall, setIsScreenSmall] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isActive, setIsActive] = useState(false);
+
+    useEffect(() => {
+        const checkScreenSize = () => {
+            setIsScreenSmall(window.innerWidth < 768);
+        };
+
+        checkScreenSize();
+        window.addEventListener('resize', checkScreenSize);
+
+        return () => window.removeEventListener('resize', checkScreenSize);
+    }, []);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+        setIsActive(!isActive);
+    };
 
     const handleLogout = () => {
         logout();
@@ -13,22 +36,38 @@ function NavBar() {
 
     return (
         <nav>
-            <Link to="/">About</Link>
-            <Link to="/auth/dfacs">DFacs</Link>
-            <Link to="/">Contact</Link>
-            {isLoggedIn ? (
-                <>
-                    <Link to="/profile">Profile</Link>
-                    <span>Welcome, {currentUser}</span>
-                    <button onClick={handleLogout}>Logout</button>
-                </>
-            ) : (
-                <>
-                    <Link to="/auth/customer/login">Customer login</Link>
-                    <Link to="/auth/92G/login">Culinarians login</Link>
-                    <Link to="/auth/register/customer">Sign up</Link>
-                </>
-            )}
+            <div className="logo">
+                <img src={logo} alt="logo" />
+                <img src={divLogo} alt="logo" />
+                <img src={healthLogo} alt="logo" />
+            </div>
+            <div className="nav-links">
+                {isScreenSmall && (
+                    <button className="hamburger-menu-button" onClick={toggleMenu}>
+                        <div className={`hamburger-menu-icon ${isActive ? 'active' : ''}`}></div>
+                    </button>
+                )}
+                {(isScreenSmall && isMenuOpen) || !isScreenSmall ? (
+                    <>
+                        <Link to="/">About</Link>
+                        <Link to="/auth/dfacs">DFacs</Link>
+                        <Link to="/">Contact</Link>
+                        {isLoggedIn ? (
+                            <>
+                                <Link to="/profile">Profile</Link>
+                                <span>Welcome, {currentUser}</span>
+                                <button onClick={handleLogout}>Logout</button>
+                            </>
+                        ) : (
+                            <>
+                                <Link to="/auth/customer/login">Customer login</Link>
+                                <Link to="/auth/92G/login">Culinarians login</Link>
+                                <Link to="/auth/register/customer">Sign up</Link>
+                            </>
+                        )}
+                    </>
+                ) : null}
+            </div>
         </nav>
     );
 }
