1f82d99b2b57b9c7e968f25e349f03923b26202f
Crissy Cabrera <crissymichelle@proton.me>
2024-01-28 07:57:55 -1000

    started NavBar and HomeRoute components

--- src/App.js
+++ src/App.js
@@ -1,47 +1,22 @@
 import React from 'react';
 import './App.css';
-import culinaryImg from './images/culinary1.png';
-
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { AuthProvider, useAuth } from './components/AuthContext';
 import AuthRoute from './routes/AuthRoute';
+import NavBar from './components/NavBar';
+import HomeRoute from './routes/HomeRoute';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2><i>Welcome to dfac Dash</i></h2>
-        <h1>Passionate about good food and pro service</h1>
-        <img src={culinaryImg} className="App-logo" alt="logo" />
-        <p>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Menus
-        </a>
-      </header>
-      {/* <p>
-          <i>About DFACdash</i><br></br>
-
-          The meal prep program was started by a group of food service culinarians who
-          who wanted to solve the customer problem on Schofield Barracks of getting the
-          right meal with the right nutrition using a Soldier's meal card to supplement
-          their lifestyle.
-        </p> */}
-      <AuthProvider>
-        <Router>
-        <Link to="/auth/customer/login">Go to Auth Route</Link>
+    <AuthProvider>
+      <Router>
+          <NavBar />
           <Routes>
             <Route path="/auth/customer/login" element={<AuthRoute />} />
-            
+            <Route path="/" element={<HomeRoute />} />
           </Routes>
         </Router>
       </AuthProvider>
-    </div>
   );
 }
 
