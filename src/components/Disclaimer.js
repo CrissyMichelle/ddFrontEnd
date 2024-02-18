c1d942f63bc884969334873ca2998fe2a6a760e2
Crissy Cabrera <crissymichelle@proton.me>
2024-02-18 13:00:56 -1000

    started disclaimer banner component

---
+++ src/components/Disclaimer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Disclaimer = () => {
+    return (
+        <div className="disclaimer-banner">
+            <p>
+                <strong>Notice:</strong> This application is for demo purposes without robust cybersecurity assurances. TEST DATA ONLY
+            </p>
+        </div>
+    );
+}
+
+export default Disclaimer;
